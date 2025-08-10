67c09a96641de793f12497bf1b4293c5ab32a61c
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-08-10 03:30:37 -0300

    style: form-page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { ListMovies } from '../../components/list-movies/list-movies';
+import { Form } from '../../components/form/form';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, ListMovies],
+  imports: [Header, ListMovies, Form],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
