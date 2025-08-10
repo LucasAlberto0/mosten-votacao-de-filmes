ca6130f5669f34b4a5e52a8cd02df9d269c48069
LucasAlberto0 <lucasalberto4321@gmail.com>
2025-08-10 16:42:01 -0300

    style: home-responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('mosten-votacao-de-filmes');
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-  ngOnInit(): void{
-    AOS.init();
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
   }
 }
